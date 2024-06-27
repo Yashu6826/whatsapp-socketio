0fce54939e8d899edf4cf202ee176b4dcd5bc19b
Yashu6826 <yashrajgupt6826a@gmail.com>
2024-06-28 02:23:14 +0530

    djjd

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT=process.env.PORT||9000
 const io = new Server(PORT, {
    
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://playful-pithivier-2ebb2d.netlify.app/',
     }, 
 })
 
