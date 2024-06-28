88c6a61e359deb0a86ac8b64188dab31f9c2a09d
Yashu6826 <yashrajgupt6826a@gmail.com>
2024-06-28 18:16:10 +0530

    ddd

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
 import { Server } from 'socket.io';
 
-
-
-
-
-const PORT=process.env.PORT||9000
+const PORT = process.env.PORT || 9000;
 const io = new Server(PORT, {
-   
+  
     cors: {
         origin: '*',
-    }, 
-})
-
+        methods: ["GET", "POST"]
+    },
+});
 
 let users = [];
 
 const addUser = (userData, socketId) => {
     !users.some(user => user.sub === userData.sub) && users.push({ ...userData, socketId });
-}
+};
 
 const removeUser = (socketId) => {
     users = users.filter(user => user.socketId !== socketId);
-}
+};
 
 const getUser = (userId) => {
     return users.find(user => user.sub === userId);
-}
+};
 
-io.on('connection',  (socket) => {
-    console.log('user connected')
+io.on('connection', (socket) => {
+    console.log('user connected');
 
-    //connect
+    // connect
     socket.on("addUser", userData => {
         addUser(userData, socket.id);
         io.emit("getUsers", users);
-    })
+    });
 
-    //send message
+    // send message
     socket.on('sendMessage', (data) => {
         const user = getUser(data.receiverId);
-        io.to(user.socketId).emit('getMessage', data)
-    })
+        if (user) {
+            io.to(user.socketId).emit('getMessage', data);
+        } else {
+            console.log(`User with ID ${data.receiverId} not found`);
+        }
+    });
 
-    //disconnect
+    // disconnect
     socket.on('disconnect', () => {
         console.log('user disconnected');
         removeUser(socket.id);
         io.emit('getUsers', users);
-    })
-})
+    });
+});
